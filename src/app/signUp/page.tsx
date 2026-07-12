7bf0d4fa8e44ac6a2677f265507b2ed8088daec8
Amarachi-Codes <etheldorisamarachi@gmail.com>
2026-07-13 00:42:17 +0100

    updated styling

--- src/app/signUp/page.tsx
+++ src/app/signUp/page.tsx
@@ -1,3 +1,5 @@
+import { MdKeyboardArrowLeft } from "react-icons/md";
+
 export default function Signup (){
      
     return(
@@ -5,11 +7,12 @@ export default function Signup (){
         <div>
             <div>
                 <div className="left-container">
-                    <div>
+                    <div className="flex items-center ">
+                        <MdKeyboardArrowLeft />
                         <p>Back home</p>
                     </div>
-                    <div>
-                        <div>
+                    <div className="flex gap-4">
+                        <div className="rounded-full bg-amber-300 w-8 h-8 flex items-center justify-center">
                             <p>1</p>
                         </div>
                         <div>
@@ -19,7 +22,7 @@ export default function Signup (){
                         <div>
                         <p> ----------------</p>
                         </div>
-                        <div>
+                        <div className="rounded-full bg-amber-300 w-8 h-8 flex items-center justify-center">
                             <p>2</p>
                         </div>
                         <div>
@@ -29,6 +32,25 @@ export default function Signup (){
 
                         
                     </div>
+
+                    <div>
+                        <h2>Create your Bitbandy account</h2>
+                        <p>Tell us how you plan to use Bitbandy so we can tailor your experience</p>
+                    </div>
+                    <div>
+                        <div>
+                            <p>For ticket buyers</p>
+                            <p>I want to discover and attend events</p>
+                            <p>Save favourites, manage tickets and check in seamlessly.</p>
+                        </div>
+                         <div>
+                            <p>For ticket buyers</p>
+                            <p>I want to discover and attend events</p>
+                            <p>Save favourites, manage tickets and check in seamlessly.</p>
+                        </div>
+                    </div>
+                    <button>Continue</button>
+                    <p>By continuing, you agree to Bitbandy's <span>Terms of Service</span> and <span>Privacy Policy</span>.</p>
                 </div>
                 <div className="right-container">
 
