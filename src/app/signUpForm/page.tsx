c52eb61fb0a577adffda126f683417f709323219
Amarachi-Codes <etheldorisamarachi@gmail.com>
2026-07-14 22:22:36 +0100

    upadted signin form

--- src/app/signUpForm/page.tsx
+++ src/app/signUpForm/page.tsx
@@ -9,11 +9,11 @@ export default function SignUpForm (){
     return(
         <>
         <div className="h-screen">
-                    <div className="flex flex-row justify-center items-center mx-40 my-14 h-140">
+                    <div className="flex flex-row justify-center items-center mx-40 my-14 h-180">
                         <div className="flex flex-col flex-1 gap-4 bg-offwhite border border-gray-500 p-12 rounded-l-3xl h-full">
                             <div className="flex items-center ">
                                 <MdKeyboardArrowLeft />
-                                <Link href="/signUp"><p>Back home</p></Link>
+                                <Link href="/"><p>Back home</p></Link>
                             </div>
                             <div className="flex gap-4">
                                 <div className="rounded-full bg-gray-200 w-8 h-8 flex items-center justify-center">
@@ -41,15 +41,45 @@ export default function SignUpForm (){
                                 <h2 className="text-2xl font-semibold">Create your account</h2>
                                 <p className="text-gray-600">Sign up to start discovering curated experiences near you on Bitbandy.</p>
                             </div>
+                            <div>
+                            <div className="flex gap-4 my-4 ">
+                                <div className="flex flex-col gap-2 w-full">
+                                    <label htmlFor="email">Email</label>
+                                    <input type="email" id="email" placeholder="Enter your email" className="border border-gray-300 rounded-lg p-2 placeholder:px-4 focus:border-purple-500 focus:outline-none focus:ring focus:ring-purple-500" />
+                                </div>
+                                <div className="flex flex-col gap-2 w-full">
+                                    <label htmlFor="firstname">First Name</label>
+                                    <input type="text" id="firstname" placeholder="Enter your first name" className="border border-gray-300 rounded-lg p-2 placeholder:px-4 focus:border-purple-500 focus:outline-none focus:ring focus:ring-purple-500" />
+                                </div>
+                            </div>
                             <div className="flex gap-4 my-4">
+                                <div className="flex flex-col gap-2 w-full">
+                                    <label htmlFor="lastname">Last Name</label>
+                                    <input type="text" id="lastname" placeholder="Enter your last name" className="border border-gray-300 rounded-lg p-2 placeholder:px-4 focus:border-purple-500 focus:outline-none focus:ring focus:ring-purple-500" />
+                                </div>
+                                <div className="flex flex-col gap-2 w-full">
+                                    <label htmlFor="password">Password</label>
+                                    <input type="password" id="password" placeholder="Enter your password" className="border border-gray-300 rounded-lg p-2 placeholder:px-4 focus:border-purple-500 focus:outline-none focus:ring focus:ring-purple-500" />
+                                </div>
                                 
                             </div>
-                            <Link href="/signUpForm"><button className="bg-purple-500 w-full text-white rounded-lg p-2" >Register now</button></Link>
+
+                            <div className="flex gap-4 my-4">
+                                
+                                <div className="flex flex-col gap-2 w-1/2">
+                                    <label htmlFor="confirmpassword">Confirm Password</label>
+                                    <input type="password" id="confirmpassword" placeholder="Confirm your password" className="border border-gray-300 rounded-lg p-2 placeholder:px-4 focus:border-purple-500 focus:outline-none focus:ring focus:ring-purple-500" />
+                                </div>
+                                
+                            </div>
+                            </div>
+                            <Link href="//signUp"><button className="bg-purple-500 w-full text-white rounded-lg p-2" >Register now</button></Link>
                             <div className="text-center">
                                 <p>Already have an account? <span onClick={()=> setIsOpen(true)} className="text-purple-500 cursor-pointer">Sign in</span></p>
                                 <SignInModal isOpen={isOpen} 
                                           onClose={() => setIsOpen(false)} />
                             </div>
+                            <div className="mx-8 my-4"><Link href="/signUp" className="text-purple-500">Back</Link></div>
                             <p className="text-xs text-gray-500">
                                 By continuing, you agree to Bitbandy's <span className="text-purple-500 ">Terms of Service</span> and <span className="text-purple-500">Privacy Policy</span>.
                             </p>
